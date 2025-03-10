01c4a50df6e5706b7cad59600c386ea5bd47cc9b
Leontievna <janesel@ya.ru>
2025-03-10 21:41:45 +0100

    Added allure attributes

--- PageObjects/OnlineHighSchoolPageObject.cs
+++ PageObjects/OnlineHighSchoolPageObject.cs
@@ -6,7 +6,7 @@ using OpenQA.Selenium.Chrome;
 
 namespace techTask;
 
-[TestFixture]
+[AllureNUnit]
 public class OnlineHighSchoolPageObject(ChromeDriver driver) : BasePage(driver)
 {
     string miaprepUrl = "https://miaprep.com/online-school/";
